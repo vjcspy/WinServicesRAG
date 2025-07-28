50857542abc4f3145bf5f32f1c1f5097cf50092b
win <mr.vjcspy@gmail.com>
2025-07-28 22:42:58 +0700

    feat: update log file path in Program.cs for WatchdogService logging

--- src/WatchDogService/Program.cs
+++ src/WatchDogService/Program.cs
@@ -84,7 +84,7 @@ public class Program
                     .WriteTo.Console(
                         outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
                     .WriteTo.File(
-                        Path.Combine(logPath, "watchdog-.log"),
+                        Path.Combine(logPath, $@"{WatchdogServiceConfig.WatchdogLogFileName}-.log"),
                         rollingInterval: RollingInterval.Day,
                         retainedFileCountLimit: 7,
                         outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
